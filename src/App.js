f9dadf4f915b76571b11b002be192781e13fe408
Hanna Boratynska <boratynskahanna@gmail.com>
2021-07-12 08:37:47 +0200

    Add status change of the table - not working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Dashboard from './components/views/Dashboard/Dashboard';
 import Login from './components/views/Login/Login';
-import Waiter from './components/views/Waiter/Waiter';
+import Waiter from './components/views/Waiter/WaiterContainer.js';
 import Tables from './components/views/Tables/Tables';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import { StylesProvider } from '@material-ui/core/styles';
@@ -12,7 +12,8 @@ import { ThemeProvider } from '@material-ui/styles';
 import Booking from './components/views/Booking/Booking';
 import Order from './components/views/Order/Order';
 import Event from './components/views/Event/Event';
-
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const theme = createMuiTheme({
   palette: {
@@ -24,24 +25,27 @@ const theme = createMuiTheme({
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={process.env.PUBLIC_URL + '/'} component={Dashboard} />
-              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={Event} />
-              <Route exact path={process.env.PUBLIC_URL + '/order/:id'} component={Order} />
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StylesProvider>
-    </BrowserRouter>
+      <Provider store={store}>
 
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={process.env.PUBLIC_URL + '/'} component={Dashboard} />
+                <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+                <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={Event} />
+                <Route exact path={process.env.PUBLIC_URL + '/order/:id'} component={Order} />
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </Provider>
+
+    </BrowserRouter>
 
   );
 }
---
+++ src/redux/tablesRedux.js
@@ -0,0 +1,87 @@
+import Axios from 'axios';
+import { api } from '../settings';
+
+/* selectors */
+export const getAll = ({tables}) => tables.data;
+export const getLoadingState = ({tables}) => tables.loading;
+
+/* action name creator */
+const reducerName = 'tables';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+const FETCH_START = createActionName('FETCH_START');
+const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
+const FETCH_ERROR = createActionName('FETCH_ERROR');
+const FETCH_STATUS = createActionName('FETCH_STATUS');
+
+/* action creators */
+export const fetchStarted = payload => ({ payload, type: FETCH_START });
+export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
+export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+export const fetchStatus = payload => ({ payload, type: FETCH_STATUS });
+
+/* thunk creators */
+export const fetchFromAPI = () => {
+  return (dispatch, getState) => {
+    dispatch(fetchStarted());
+
+    Axios
+      .get(`${api.url}/api/${api.tables}`)
+      .then(res => {
+        dispatch(fetchSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchError(err.message || true));
+      });
+  };
+};
+
+// TO DO 
+export const fetchStatusFromAPI = () => {
+  
+};
+
+/* reducer */
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case FETCH_START: {
+      return {
+        ...statePart,
+        loading: {
+          active: true,
+          error: false,
+        },
+      };
+    }
+    case FETCH_SUCCESS: {
+      return {
+        ...statePart,
+        loading: {
+          active: false,
+          error: false,
+        },
+        data: action.payload,
+      };
+    }
+    case FETCH_ERROR: {
+      return {
+        ...statePart,
+        loading: {
+          active: false,
+          error: action.payload,
+        },
+      };
+    }
+
+    // TO DO 
+    case FETCH_STATUS: {
+      return {
+        ...statePart, 
+
+      };
+    }    
+    default:
+      return statePart;
+  }
+}
